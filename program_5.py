fb62fdcca9a158ae45c5175bf6b1e018c98e200f
JosephRydberg <jwrydberg@students.unwsp.edu>
2024-09-16 20:28:55 -0500

    project fix

--- program_5.py
+++ program_5.py
@@ -1,12 +1,4 @@
 #Hot Dog Price Calculator Joseph Rydberg 9/16/24
-# There are two kinds of hot dogs sold:
-# Hot Dog ($3.50), Chili Dog ($4.50).  
-# Additionally a person can order cheese ($0.50), peppers ($0.75), or grilled onions ($1.00).  
-# Also tax is 7%. 
-# Write a program the inputs the type of hot dog wanted and optional toppings.  
-# The program then displays the hot dog cost, tax and total cost.
-from itertools import filterfalse
-
 
 def price_calculator(cost):
     cost = 0.0
