3c7a8c8d6b4b6f7ea5d5c7d6b0cf84d526093d4c
JosephRydberg <jwrydberg@students.unwsp.edu>
2024-09-16 20:28:29 -0500

    Assignment

--- program_3.py
+++ program_3.py
@@ -1,26 +1,19 @@
-# Programming Excersize 3-13
-
-# The Fast Freight Shipping Company charges the following rates:
-
-# Weight    	Price Per Pound
-# 2 pounds or less   	$1.50
-# Over 2 pounds but not more than 6 pounds  	$3.00
-# Over 6 pounds but not more than 10 pounds	$4.00
-# Over 10 pounds	$4.75
-# Write a program which calculates the shipping charge and displays the total.
+# Freight Cost Calculator Joseph Rydberg 9/16/24
 
 def weight_conversion(weight):
-    # Calculate the shipping charge.
     shippingCost = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
+
+    if weight <= 2:
+        shippingCost = 1.50
+    elif weight > 2 and weight <= 6:
+        shippingCost = 3.00
+    elif weight > 6 and weight <= 10:
+        shippingCost = 4.00
+    elif weight > 10:
+        shippingCost = 4.75
     
     return shippingCost
 
-#### This piece of the code has been done for you,
-#### you only need to worry about the actual shipping 
-#### charge logic in the weight_conversion function
 if __name__ == '__main__':
     # Local variables
     weight = 0.0
